1731d5429023a5ad1e34ad74cfd6e556eea3b8e1
khk811 <hyunkyungkim811@gmail.com>
2021-11-22 17:16:24 +0900

    don't know how but somehow is working

--- Libft/memset/ft_memset.c
+++ Libft/memset/ft_memset.c
@@ -2,15 +2,18 @@
 #include <string.h>
 
 // https://st-lab.tistory.com/198
+// https://dojang.io/mod/page/view.php?id=552; void pointer as a parameter in a function.
 
 void	*ft_memset(void *dest, int c, size_t len)
 {
 	unsigned char	casted_c;
 	size_t i;
 
+	i = 0;
 	casted_c = (unsigned char)c;
 	while (i < len)
-		dest[i++] = casted_c;
+		*(char *)(dest + (i++)) = casted_c;
+	// how the fuck did i write this wft why is this working????
 	return (dest);
 }
 
@@ -19,9 +22,9 @@ int	main()
 	int	memset_Ptr[10];
 	int	ft_memset_Ptr[10];
 
-	memset(memset_Ptr, 0, 10 * sizeof(int));
-	ft_memset(ft_memset_Ptr, 0, 10 *sizeof(int));
-	for (int i; i < 10; i++)
+	memset(memset_Ptr, 1, 10 * sizeof(int));
+	ft_memset(ft_memset_Ptr, 1, 10 *sizeof(int));
+	for (int i = 0; i < 10; i++)
 	{
 		printf("memset: %d ", memset_Ptr[i]);
 		printf("ft_memset: %d\n", ft_memset_Ptr[i]);
