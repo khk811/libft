e713d3ae86b2eab24496fee7fe146fadc8715ac6
khk811 <hyunkyungkim811@gmail.com>
2021-12-06 13:54:53 +0900

    type casted inputs

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -3,12 +3,16 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned char	*casted_s1;
+	unsigned char	*casted_s2;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
+	casted_s1 = (unsigned char *)s1;
+	casted_s2 = (unsigned char *)s2;
+	while ((casted_s1[i] || casted_s2[i]) && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (casted_s1[i] != casted_s2[i])
+			return (casted_s1[i] - casted_s2[i]);
 		i++;
 	}
 	return (0);
