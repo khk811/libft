9f75e929d7b771af8a0d7968c9ebedb3083b33c5
khk811 <hyunkyungkim811@gmail.com>
2021-12-06 11:36:43 +0900

    add func. to header

--- Libft/libft.h
+++ Libft/libft.h
@@ -7,11 +7,32 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
-int	ft_strlen(char *str);
-char	*ft_strdup(char *src);
+int ft_isascii(int c);
+int ft_isprint(int c);
+size_t ft_strlen(const char *s);
+void    *ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+void	*memmove(void *dst, const void *src, size_t len);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dest, char *src, size_t dest_size);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strrchr(const char *s, int c);
+int ft_strncmp(char *s1, char *s2, size_t n);
+void    *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+int ft_atoi(char *str);
+void    *ft_calloc(size_t count, size_t size);
+char	*ft_strdup(char *s1);
 
 /**addtional function*/
+char *ft_strtrim(char const *s1, char const *set);
+char *ft_strjoin(char const *s1, char const *s2);
+char **ft_split(const char *s, char c);
+char *ft_itoa(int n);
 void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
 void ft_putendl_fd(char *s, int fd);
