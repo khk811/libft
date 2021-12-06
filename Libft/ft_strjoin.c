4e4a5d848ce16914539cbe04511be6b839cbd85d
khk811 <hyunkyungkim811@gmail.com>
2021-12-06 12:20:56 +0900

    rm main, add libft.h

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -1,49 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	int	i;
-
-	i = 0;
-	while (*src && --dstsize)
-		*dst++ = *src++;
-	if (dstsize > 0)
-		*dst = '\0';
-	return (ft_strlen(src));
-}
-
-size_t	ft_strlcat(char *dest, char *src, size_t dest_size)
-{
-	int	src_len_to_add;
-	int	idx_dest;
-	int	idx_src;
-	int	normal_return;
-
-	src_len_to_add = (int)(dest_size - ft_strlen(dest)) - 1;
-	idx_dest = ft_strlen(dest);
-	idx_src = 0;
-	normal_return = (int)(ft_strlen(dest) + ft_strlen(src));
-	if (src_len_to_add <= 0)
-		return (ft_strlen(src) + dest_size);
-	while (src[idx_src] != '\0' && src_len_to_add > 0)
-	{
-		dest[idx_dest++] = src[idx_src++];
-		src_len_to_add--;
-	}
-	dest[idx_dest] = '\0';
-	return (normal_return);
-}
+#include "libft.h"
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
@@ -55,13 +11,4 @@ char *ft_strjoin(char const *s1, char const *s2)
 	ft_strlcpy(result, (char *)s1, ft_strlen(s1) + 1);
 	ft_strlcat(result, (char *)s2, ft_strlen(s2) + ft_strlen(result) + 1);
 	return (result);
-}
-
-int	main()
-{
-	char	*the_sauce = "sriracha";
-	char	*the_love = ", with love.";
-
-	printf("the result: %s\n", ft_strjoin(the_sauce, the_love));
-	return (0);
 }
