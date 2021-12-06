cd8afb481b660144dd0ffc519a92f28595b803ea
khk811 <hyunkyungkim811@gmail.com>
2021-12-06 12:16:15 +0900

    copy memcpy. need revise

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	//what's the diff. w/memmove and memcpy??
-}
+	size_t	i;
 
-int	main()
-{
-	char	dest[35] = "ok, something in the destination";
-	char	source[25] = "from source, with love";
-
-	printf("memmove result: %s\n", memmove(dest, source, 12));
-	return (0);
+	i = 0;
+	while (i < len)
+	{
+		*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
+		i++;
+	}
+	return (dst);
 }
