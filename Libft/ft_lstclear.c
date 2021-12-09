089417f8f97da9001f88b4702e6c19a3d86494ed
khk811 <hyunkyungkim811@gmail.com>
2021-12-09 22:00:41 +0900

    doesnt work - need revise

---
+++ Libft/ft_lstclear.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+  t_list    *lst_element;
+
+  lst_element = *lst;
+  while (lst_element)
+  {
+    (*del)(lst_element->content);
+    lst_element = lst_element->next;
+    free(lst_element);
+  }
+}
