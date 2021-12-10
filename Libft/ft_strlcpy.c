b4463189be74199c8a8f147a8d37ec0146d28a08
khk811 <hyunkyungkim811@gmail.com>
2021-12-10 18:24:23 +0900

    add cond. and modify loop cond.

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -2,12 +2,14 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	int	i;
+	size_t	return_value;
 
-	i = 0;
+	return_value = ft_strlen(src);
+	if (!dstsize)
+		return (return_value);
 	while (*src && --dstsize)
 		*dst++ = *src++;
-	if (dstsize > 0)
+	if (dstsize >= 0)
 		*dst = '\0';
-	return (ft_strlen(src));
+	return (return_value);
 }
