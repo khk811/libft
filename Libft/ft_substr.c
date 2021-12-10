99fbdbfe1fd056143d49010879a7b7be6725dbed
khk811 <hyunkyungkim811@gmail.com>
2021-12-10 21:33:52 +0900

    substr added

---
+++ Libft/ft_substr.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, &s[start], (len + 1));
+	return (result);
+}
