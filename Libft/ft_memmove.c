1e7264181475048802c2e4500e65021996fdb965
khk811 <hyunkyungkim811@gmail.com>
2021-12-10 17:53:52 +0900

    it works - need to know why

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -1,14 +1,22 @@
-#include <stdlib.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < len)
+	if (dst < src)
 	{
-		*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
-		i++;
+		while (i < len)
+		{
+			*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
+			i++;
+		}
+	}
+	else if (dst > src)
+	{
+		while (len-- > 0)
+			*(unsigned char *)(dst + len) = *(unsigned char *)(src + len);
 	}
 	return (dst);
 }
