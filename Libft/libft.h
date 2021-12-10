895474c45e8307bc519ae1932fb8465b68109c62
khk811 <hyunkyungkim811@gmail.com>
2021-12-10 21:29:49 +0900

    add substr

--- Libft/libft.h
+++ Libft/libft.h
@@ -35,6 +35,7 @@ void    *ft_calloc(size_t count, size_t size);
 char	*ft_strdup(char *s1);
 
 /**addtional function*/
+char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strtrim(char const *s1, char const *set);
 char *ft_strjoin(char const *s1, char const *s2);
 char **ft_split(const char *s, char c);
