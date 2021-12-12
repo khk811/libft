5268f123e59fcc14e5ccdb63dd86dfe95d506940
khk811 <hyunkyungkim811@gmail.com>
2021-12-12 11:11:53 +0900

    modify alloc and count method

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -8,11 +8,16 @@ static int	ft_count_words(const char *s, char c)
 	word_num = 0;
 	while (*s)
 	{
-		if (*s == c)
+		if (*s && *s != c)
+        {
 			word_num++;
+            while (*s && *s != c)
+                s++;
+            if (!*s)
+                break;
+        }
 		s++;
 	}
-	word_num++;
 	return (word_num);
 }
 
@@ -22,20 +27,27 @@ static char	**ft_assign_words(const char *s, char c, char **arr)
 	size_t	the_char_num;
 	char	*start_of_the_word;
 
-	start_of_the_word = (char*)s;
 	i = 0;
-	the_char_num = 2;
+	the_char_num = 0;
 	while (*s)
 	{
-		if (*(s + 1) == c || *(s + 1) == '\0')
+		if (*s && *s != c)
 		{
+            start_of_the_word = (char*)s;
+            while (*s && *s != c)
+            {
+                the_char_num++;
+                s++;
+            }
 			arr[i] = (char *)malloc(sizeof(char) * (the_char_num + 1));
-			ft_strlcpy(arr[i++], start_of_the_word, the_char_num);
-			start_of_the_word = (char *)(s + 2);
+            if (!arr[i])
+                return (NULL);
+			ft_strlcpy(arr[i++], start_of_the_word, (the_char_num + 1));
 			the_char_num = 0;
+            if (!*s)
+                break;
 		}
 		s++;
-		the_char_num++;
 	}
 	arr[i] = 0;
 	return (arr);
