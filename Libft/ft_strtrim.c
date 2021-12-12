afc0dd9c48af39d005659b5f9415c4534213b5eb
khk811 <hyunkyungkim811@gmail.com>
2021-12-12 11:10:01 +0900

    separate func. and trim_len methods

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -1,41 +1,47 @@
 #include <stdlib.h>
 #include "libft.h"
 
+static int  ft_trim_len(char const *s1, char const *set)
+{
+  int   i;
+  int   result;
+
+  i = 0;
+  result = (int)ft_strlen(s1);
+  while (ft_strchr(set, s1[i++]))
+  {
+    if (!result)
+      return (0);
+    result--;
+  }
+  i = (int)ft_strlen(s1);
+  while (ft_strchr(set, s1[--i]))
+    result--;
+  return (result);
+}
+
 char *ft_strtrim(char const *s1, char const *set)
 {
 	int	i;
-	int	j;
 	int	len;
 	char *result;
 
 	if (!set)
 		return (ft_strdup((char *)s1));
-	i = 0;
-	len = 0;
-	while (set[i])
-	{
-		if (!ft_strchr(s1, set[i]))
-			len++;
-		i++;
-	}
+    len = ft_trim_len(s1, set);
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (!result)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (*s1)
 	{
-		j = 0;
-		while (set[j])
-		{
-			if (s1[i] != set[j])
-			{
-				*result++ = s1[i];
-			}
-			j++;
-		}
-		i++;
-	}
-	result++;
-	*result = '\0';
+      if (ft_strchr(set, s1[i]))
+        i++;
+      else
+       {
+         ft_strlcpy(result, s1 + i, (len + 1));
+         break ;
+       }
+    }
 	return (result);
 }
