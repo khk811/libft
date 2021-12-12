47a8964f33c80ae8770c20183c309947338c40c7
khk811 <hyunkyungkim811@gmail.com>
2021-12-13 04:13:32 +0900

    s1, s2 null guard

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -5,6 +5,10 @@ char *ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
 
+    if (!s1)
+        return ((char *)s2);
+    if (!s2)
+        return ((char *)s1);
 	result = (char *)malloc(sizeof(char) * (int)(ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!result)
 		return (NULL);
