15a350794be7a01709933f1fe8a2898f4c593309
khk811 <hyunkyungkim811@gmail.com>
2021-12-13 21:13:03 +0900

    change src_add cond.

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,30 +6,30 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 14:19:19 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/13 14:19:21 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/13 18:00:44 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, char *src, size_t dest_size)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	int	src_len_to_add;
-	int	idx_dest;
+	int	idx_dst;
 	int	idx_src;
 	int	normal_return;
 
-	src_len_to_add = (int)(dest_size - ft_strlen(dest)) - 1;
-	idx_dest = ft_strlen(dest);
+	src_len_to_add = (int)(dstsize - ft_strlen(dst)) - 1;
+	idx_dst = ft_strlen(dst);
 	idx_src = 0;
-	normal_return = (int)(ft_strlen(dest) + ft_strlen(src));
-	if (src_len_to_add <= 0)
-		return (ft_strlen(src) + dest_size);
+	normal_return = (int)(ft_strlen(dst) + ft_strlen(src));
+	if (src_len_to_add < 0)
+		return (ft_strlen(src) + dstsize);
 	while (src[idx_src] != '\0' && src_len_to_add > 0)
 	{
-		dest[idx_dest++] = src[idx_src++];
+		dst[idx_dst++] = src[idx_src++];
 		src_len_to_add--;
 	}
-	dest[idx_dest] = '\0';
+	dst[idx_dst] = '\0';
 	return (normal_return);
 }
