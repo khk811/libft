8d608214b040a45cd583cab35c70d5dd78694a8d
khk811 <hyunkyungkim811@gmail.com>
2021-12-13 21:15:00 +0900

    change seq.

--- Libft/ft_lstclear.c
+++ Libft/ft_lstclear.c
@@ -3,14 +3,14 @@
 
 void ft_lstclear(t_list **lst, void (*del)(void *))
 {
-  t_list    *lst_element;
+	t_list    *next_node;
 
-  lst_element = *lst;
-  while (lst_element)
-  {
-    (*del)(lst_element->content);
-    lst_element = lst_element->next;
-    free(lst_element);
-  }
-  lst = NULL;
+	while (*lst)
+	{
+		next_node = (*lst)->next;
+		(*del)((*lst)->content);
+		free(*lst);
+		*lst = next_node;
+	}
+	lst = NULL;
 }
