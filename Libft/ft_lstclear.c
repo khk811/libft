881d0b77ce39b93828b84693e7c829faa9e22221
khk811 <hyunkyungkim811@gmail.com>
2021-12-13 21:23:43 +0900

    norm

--- Libft/ft_lstclear.c
+++ Libft/ft_lstclear.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 21:23:07 by hyunkkim          #+#    #+#             */
+/*   Updated: 2021/12/13 21:23:11 by hyunkkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "libft.h"
 
-void ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list    *next_node;
+	t_list	*next_node;
 
 	while (*lst)
 	{
