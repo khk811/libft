57ed71b78d3fd46646757261286b7e23738a076c
khk811 <hyunkyungkim811@gmail.com>
2021-12-15 21:37:44 +0900

    add new var. and use other func.

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -1,22 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 21:17:03 by hyunkkim          #+#    #+#             */
+/*   Updated: 2021/12/15 21:17:06 by hyunkkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <stdlib.h>
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list  *result;
-    int i;
+	t_list	*result;
+	t_list	*new;
 
-    if (!lst || !f)
-        return (0);
-    result = (t_list *)malloc(sizeof(t_list));
-    if (!result)
-        return (NULL);
-    i = 0;
-    while (lst)
-    {
-        result->content = (*f)(lst->content);
-        lst = lst->next;
-        result = result->next;
-    }
-    return (result);
+	if (!lst || !f)
+		return (0);
+	result = ft_lstnew((*f)(lst->content));
+	if (!result)
+		return (0);
+	lst = lst->next;
+	while (lst)
+	{
+		new = ft_lstnew((*f)(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&result, (*del));
+			return (NULL);
+		}
+		ft_lstadd_back(&result, new);
+		lst = lst->next;
+	}
+	return (result);
 }
