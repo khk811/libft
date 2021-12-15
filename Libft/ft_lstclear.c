f337f8eb0e403ddec66335487a9392516acde4ba
khk811 <hyunkyungkim811@gmail.com>
2021-12-15 21:35:14 +0900

    use delone, change var.name

--- Libft/ft_lstclear.c
+++ Libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 21:23:07 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/13 21:23:11 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/15 16:42:23 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list	*next_node;
+	t_list	*next_element;
 
 	while (*lst)
 	{
-		next_node = (*lst)->next;
-		(*del)((*lst)->content);
-		free(*lst);
-		*lst = next_node;
+		next_element = (*lst)->next;
+		ft_lstdelone(*lst, (*del));
+		*lst = next_element;
 	}
 	lst = NULL;
 }
