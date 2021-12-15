1704dcb1f1e2cfb0aa0986b41039c7aa862bfe25
khk811 <hyunkyungkim811@gmail.com>
2021-12-15 21:34:24 +0900

    null guard

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:48:45 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/13 13:49:50 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/15 13:25:48 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new;
 
 	new = malloc(sizeof(t_list));
-	if (new)
+	if (!new)
+		return (NULL);
+	else
 	{
 		new->content = content;
 		new->next = NULL;
