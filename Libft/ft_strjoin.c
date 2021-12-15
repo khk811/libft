7ee314a6a0bc851af09c77c56eeeb19da1347672
khk811 <hyunkyungkim811@gmail.com>
2021-12-15 21:45:16 +0900

    change null guard line

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 14:18:54 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/15 21:31:30 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/15 21:44:25 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*result;
 	size_t	len;
 
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	if (!s1 && !s2)
+	if (!s1 || !s2)
 		return (NULL);
 	if (!s1)
 		return ((char *)s2);
 	if (!s2)
 		return ((char *)s1);
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	result = (char *)malloc(sizeof(char) * len);
 	if (!result)
 		return (NULL);
