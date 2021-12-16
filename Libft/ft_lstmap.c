3083b751fb6dd8d56ad8153707b640203a89812b
khk811 <hyunkyungkim811@gmail.com>
2021-12-16 17:32:26 +0900

    var ret to null

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 21:17:03 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/15 21:17:06 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/16 17:23:19 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*result;
+	t_list	*ret;
 	t_list	*new;
 
 	if (!lst || !f)
 		return (0);
-	result = ft_lstnew((*f)(lst->content));
-	if (!result)
-		return (0);
-	lst = lst->next;
+	ret = NULL;
 	while (lst)
 	{
 		new = ft_lstnew((*f)(lst->content));
 		if (!new)
 		{
-			ft_lstclear(&result, (*del));
+			ft_lstclear(&ret, (*del));
 			return (NULL);
 		}
-		ft_lstadd_back(&result, new);
+		ft_lstadd_back(&ret, new);
 		lst = lst->next;
 	}
-	return (result);
+	return (ret);
 }
