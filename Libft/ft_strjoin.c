57f3f491d918d87afda7ae1f3737193bb39f960d
khk811 <hyunkyungkim811@gmail.com>
2021-12-17 20:21:37 +0900

    null guard cond. changed

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,29 +6,29 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 14:18:54 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/15 21:44:25 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/17 20:15:58 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
+	char	*ret;
 	size_t	len;
 
 	if (!s1 || !s2)
-		return (NULL);
-	if (!s1)
-		return ((char *)s2);
-	if (!s2)
-		return ((char *)s1);
+	{
+		if (!s1)
+			return ((char *)s2);
+		else if (!s2)
+			return ((char *)s1);
+	}
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	result = (char *)malloc(sizeof(char) * len);
-	if (!result)
+	ret = (char *)malloc(sizeof(char) * len);
+	if (!ret)
 		return (NULL);
-	ft_strlcpy(result, (char *)s1, ft_strlen(s1) + 1);
-	ft_strlcat(result, (char *)s2, ft_strlen(s2) + ft_strlen(result) + 1);
-	return (result);
+	ft_strlcpy(ret, (char *)s1, ft_strlen(s1) + 1);
+	ft_strlcat(ret, (char *)s2, ft_strlen(s2) + ft_strlen(ret) + 1);
+	return (ret);
 }
