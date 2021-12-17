b3226998cbf4f9c1c5a21867d3f251ea26371bfa
khk811 <hyunkyungkim811@gmail.com>
2021-12-17 20:25:18 +0900

    str to const str

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,14 +6,14 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 14:14:58 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/13 14:15:00 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/17 20:23:38 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char	*ft_strdup(char *s1)
+char	*ft_strdup(const char *s1)
 {
 	 int	i;
 	char	*result;
