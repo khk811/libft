b823c9492febe84d561249489f7ae5bf89814b6d
khk811 <hyunkyungkim811@gmail.com>
2021-12-17 16:10:08 +0900

    int typecasting and var name changed

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:52:18 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/13 13:52:23 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/17 15:53:21 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	       size_t	i;
+	unsigned char	*uc_s1;
+	unsigned char	*uc_s2;
 
 	i = 0;
+	uc_s1 = (unsigned char *)s1;
+	uc_s2 = (unsigned char *)s2;
 	while (i < n)
 	{
-		if (*(unsigned char *)(s1 + i) != *(unsigned char *)(s2 + i))
-			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+		if (uc_s1[i] != uc_s2[i])
+			return ((int)(uc_s1[i] - uc_s2[i]));
 		i++;
 	}
 	return (0);
