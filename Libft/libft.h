585d325816436ff8ed47d0635dcfd14259e083d4
khk811 <hyunkyungkim811@gmail.com>
2021-12-17 18:13:32 +0900

    rm stddef.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,14 +6,13 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 14:41:23 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/17 16:09:13 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/17 17:08:28 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
 # include <unistd.h>
 # include <stdlib.h>
 
@@ -44,7 +43,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(char *s1);
+char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strjoin(char const *s1, char const *s2);
