3cd55a5f6cbda5932b98ff9ad1dcadcaead1aa09
khk811 <hyunkyungkim811@gmail.com>
2021-12-17 15:39:38 +0900

    casted to uc - change var name

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 14:24:47 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/13 14:24:52 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/17 15:34:03 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	       size_t	i;
-	unsigned char	*casted_s1;
-	unsigned char	*casted_s2;
+	unsigned char	*uc_s1;
+	unsigned char	*uc_s2;
 
 	i = 0;
-	casted_s1 = (unsigned char *)s1;
-	casted_s2 = (unsigned char *)s2;
-	while ((casted_s1[i] || casted_s2[i]) && i < n)
+	uc_s1 = (unsigned char *)s1;
+	uc_s2 = (unsigned char *)s2;
+	while ((uc_s1[i] || uc_s2[i]) && i < n)
 	{
-		if (casted_s1[i] != casted_s2[i])
-			return (casted_s1[i] - casted_s2[i]);
+		if (uc_s1[i] != uc_s2[i])
+			return (uc_s1[i] - uc_s2[i]);
 		i++;
 	}
 	return (0);
