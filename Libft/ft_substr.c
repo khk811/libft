7e96e73983deef679e6d5f87a85a18a18e64b9b7
khk811 <hyunkyungkim811@gmail.com>
2021-12-18 15:26:21 +0900

    change if cond. seq.

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hyunkkim <hyunkkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 14:28:22 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/18 13:16:13 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/18 15:07:23 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s + start) <= len)
+	if (start >= ft_strlen(s))
+		return ((char *)ft_calloc(sizeof(char), 1));
+	if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (!result)
 		return (NULL);
-	if (start >= ft_strlen(s))
-		result[0] = '\0';
-	else
-		ft_strlcpy(result, &s[start], (len + 1));
+	ft_strlcpy(result, &s[start], (len + 1));
 	return (result);
 }
