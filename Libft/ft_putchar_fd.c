b2bcd0abde8d58d9ece8ee74f8b6096c1491f18c
khk811 <hyunkyungkim811@gmail.com>
2021-12-18 19:54:25 +0900

    rm unistd add libft.h

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -6,11 +6,11 @@
 /*   By: hyunkkim <hyunkkim@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:54:43 by hyunkkim          #+#    #+#             */
-/*   Updated: 2021/12/13 13:54:44 by hyunkkim         ###   ########.fr       */
+/*   Updated: 2021/12/18 19:54:06 by hyunkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
